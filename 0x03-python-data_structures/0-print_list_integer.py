9f0e493366cbccec585911a4555cb3ed7a7dcdab
toqa <toqaezzatly@gmail.com>
2024-01-08 21:59:15 -0500

    commit

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# 0-print_list_integer.py
+
+def print_list_integer(my_list=[]):
+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
+    """Print all integers of a list."""
+    for i in range(len(my_list)):
+        print("{:d}".format(my_list[i]))#!/usr/bin/python3
+# 0-print_list_integer.py
+
+def print_list_integer(my_list=[]):
+    """Print all integers of a list."""
+    for i in range(len(my_list)):
+        print("{:d}".format(my_list[i]))
