dc2d5dbfc62958b23012c07b5d49b5274d7aac63
toqa <toqaezzatly@gmail.com>
2024-01-12 17:27:33 -0500

    commit

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str):
+        return 0
+
+    roman_to_int = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    integer = 0
+    for i in range(len(roman_string)):
+        if i > 0 and roman_to_int[roman_string[i]] > roman_to_int[roman_string[i - 1]]:
+            integer += roman_to_int[roman_string[i]] - 2 * roman_to_int[roman_string[i - 1]]
+        else:
+            integer += roman_to_int[roman_string[i]]
+    return integer
 
