22eb898f3738139549d3dba49594432dcc8f26e6
root <toqaezzatly@gmail.com>
2024-04-14 08:59:14 -0700

    0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -32,7 +32,7 @@ def safe_print_list(my_list=[], x=0):
     try:
         printed_count = 0
         for i in range(x):
-            print(my_list[i], end=' ')
+            print(my_list[i], end='')
             printed_count += 1
         print()  # Add a newline after printing elements
         return printed_count
