f1204bc5dcce7464fa958a11921611020dfa4e33
root <toqaezzatly@gmail.com>
2024-07-04 10:12:38 -0700

    display

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-#Takes the URL and sends GET request to it and displays 200 status code reponse
-curl -sL "$l"
+# sends a GET request to a URL, and displays the body of the response only if the status code is 200
+curl -sL -w "%{http_code}" "$1" -o temp.txt | { read status; [ "$status" -eq 200 ] && cat temp.txt; }
+
