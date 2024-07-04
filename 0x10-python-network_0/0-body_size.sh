e6d93a998599c6f6f9a38624e4eded756dd54cdf
root <toqaezzatly@gmail.com>
2024-07-04 09:53:51 -0700

    display the body

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
-# This script takes in a URL, sends a request to that URL, and displays the size of the body of the response in bytes
-
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 URL"
-    exit 1
-fi
-
-url=$1
-
-# Use curl to get the size of the response body
-curl -s -o /dev/null -w "%{size_download}\n" "$url"
-
+# sends a request an URL, and displays the size of the body of the response
+curl -s "$1" | wc -c
---
+++ 0x10-python-network_0/1-body.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#Takes the URL and sends GET request to it and displays 200 status code reponse
+curl -sL "$l"
