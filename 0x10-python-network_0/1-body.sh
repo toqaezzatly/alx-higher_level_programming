14af9bc7a71b481c30a30eee80350b5168304d18
root <toqaezzatly@gmail.com>
2024-07-05 20:51:46 -0700

    fourth line is remove

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # sends a GET request to a URL, and displays the body of the response only if the status code is 200
 curl -sL -w "%{http_code}" "$1" -o temp.txt | { read status; [ "$status" -eq 200 ] && cat temp.txt; }
-
---
+++ 0x10-python-network_0/4-header.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Sends a GET request to a URL with a custom header and displays the body of the response
+curl -sH "X-School-User-Id: 98" "$1"
+
