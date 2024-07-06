e22edfb030d631fae97c89817812e23ccca0bc29
root <toqaezzatly@gmail.com>
2024-07-05 20:58:04 -0700

    Yarab Ne5lSssssw

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Sends a POST request to a URL with specific parameters and displays the body of the response
 curl -s -X POST -d "email=test@gmail.com" -d "subject=I will always be here for PLD" "$1"
-
