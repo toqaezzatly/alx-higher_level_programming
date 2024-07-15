bd1b40f391de8ad836470dea898fd78c3a121a9f
root <toqaezzatly@gmail.com>
2024-07-15 10:09:08 -0700

    check this 2

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,2 +1,3 @@
 -- Deletes the database if exists
+
 DROP DATABASE IF EXISTS htbn_0c_0;
---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,3 @@
+-- Creates table with column of name and id --
+
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
